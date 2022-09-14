4db7be241389479665845c14592eb4695dbcc717
Evan Reen <74310324+EvanReen@users.noreply.github.com>
2022-09-14 09:46:34 +0100

    Add files via upload

---
+++ Temperature/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Temperature
+{
+    private decimal temp;
+
+    public Temperature(decimal temperature)
+    {
+        this.temp = temperature;
+    }
+
+    public override string ToString()
+    {
+        return this.temp.ToString("N1") + "°C";
+    }
+}
+
+public class Display
+{
+    public static void Main()
+    {
+        Temperature currentTemperature = new Temperature(23.6m);
+        Console.WriteLine($"The current temperature is {currentTemperature}");
+    }
+}
